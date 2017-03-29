94d39b2602e35ab7c056d3f028fde4c6fdf0d87c
NaskaMimi <fedotowa.loveincity16@yandex.ru>
2017-03-29 22:16:40 +0300

    yay

--- client/src/main/ts/app/app.component.ts
+++ client/src/main/ts/app/app.component.ts
@@ -7,4 +7,8 @@ import '../assets/css/styles.css';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+    say() {
+        alert("Yay!")
+    }
+}
