2646edb28689a3a07ed603680b4b2995cb76ef86
NaskaMimi <fedotowa.loveincity16@yandex.ru>
2017-03-31 12:09:08 +0400

    wallpapers + floor

--- client/src/main/ts/app/app.component.ts
+++ client/src/main/ts/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit, Injectable } from '@angular/core';
 import '../assets/css/styles.css';
 import {WallpaperListingComponent} from './wallpaperlisting.component';
 import {MainpicComponent} from "./mainpic.component";
+import {RoomComponent} from "./room.component";
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-    providers: [ WallpaperListingComponent, MainpicComponent ]
+  providers: [ WallpaperListingComponent, MainpicComponent, RoomComponent ]
 })
 
 @Injectable()
--- client/src/main/ts/app/app.module.ts
+++ client/src/main/ts/app/app.module.ts
@@ -4,14 +4,15 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {WallpaperListingComponent} from "./wallpaperlisting.component";
 import {MainpicComponent} from "./mainpic.component";
+import {RoomComponent} from "./room.component";
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent, WallpaperListingComponent, MainpicComponent
+    AppComponent, WallpaperListingComponent, MainpicComponent, RoomComponent
   ],
-  bootstrap: [ AppComponent, WallpaperListingComponent, MainpicComponent ]
+  bootstrap: [ AppComponent, WallpaperListingComponent, MainpicComponent, RoomComponent ]
 })
 export class AppModule { }
